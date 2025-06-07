fc4ee5da85f65b2adbc5151198b0ac5716335816
stefangheorghiu03 <gheorghiu.stefan.nicolae.cngrc@gmail.com>
2025-06-07 17:32:25 +0300

    updated portal gitignore

--- portal/config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    GOOGLE_CLIENT_ID: "639744845722-ik40cdgurnaqqk4kohie7sb16jabmp6r.apps.googleusercontent.com",
-    ALLOWED_EMAIL: "gheorghiu.stefan.nicolae.cngrc@gmail.com"
-};
